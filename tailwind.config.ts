f66dbc0aa80189bcfe7d2caab4c8d452df48b9ae
dbahiense <douglas@rohling.com.br>
2024-09-09 11:53:05 -0300

    feat(tailwind): add custom colors to theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'coral-red': '#FF4040',
+        'slate-gray': '#708090',
+      }
     },
   },
   plugins: [],
